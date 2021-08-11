7601e656446433f23fcbe3561925e38e758c195d
Benjamin <dotsehyde@gmail.com>
2021-08-11 12:57:04 +0000

    :) Added Grid Layout

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Hero from "../widget/Hero";
 import NavBar from "../widget/NavBar";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles((theme) => ({
+ 
+}));
 function Home() {
+  const css = useStyles();
   return (
     <div>
       <NavBar />
-      <Hero/>
+      <Hero />
+      
+      
     </div>
   );
 }
--- src/widget/Hero.js
+++ src/widget/Hero.js
@@ -1,21 +1,17 @@
-import { makeStyles, Typography, Button } from "@material-ui/core";
+import { makeStyles, Typography, Button, Grid } from "@material-ui/core";
 import React from "react";
 
-const useStyles = makeStyles({
-  main: {
-    width: "100vw",
-    height: "30%",
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-around",
-    },
-    deco: {
-        position: "absolute",
-        top: '44%',
-        left: '30%',
-        zIndex: -1, 
-    },
+const useStyles = makeStyles((theme)=>({
+
+  deco: {
+    position: "absolute",
+    top: "44%",
+    left: "30%",
+    zIndex: -1,
+    [theme.breakpoints.down('md')]: {
+      display:'none',
+  },
+  },
   bgBanner: {
     position: "absolute",
     top: 0,
@@ -25,13 +21,23 @@ const useStyles = makeStyles({
   },
   right: {
     marginTop: "20px",
+    marginLeft: '-10px',
+    textAlign:'center'
   },
   left: {
     marginTop: "100px",
     marginLeft: "100px",
+    textAlign: 'center',
+    [theme.breakpoints.down('md')]: {
+      marginLeft: "0px",
+      marginTop:'50px',
+  },
   },
   img: {
-    height: "560px",
+    height: "40vw",
+    [theme.breakpoints.down('md')]: {
+     height:'50vw',
+  },
   },
   tagLine: {
     color: "#DF6951",
@@ -39,8 +45,19 @@ const useStyles = makeStyles({
     fontFamily: "Poppins",
     fontWeight: "700",
     textAlign: "left",
+    [theme.breakpoints.down('md')]: {
+      textAlign: "center",
+  },
   },
   heading: {
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '30px',
+      lineHeight: "39px",
+    },
+    [theme.breakpoints.down('md')]: {
+      textAlign:'center', 
+  },
+    textAlign:'left', 
     fontFamily: "Poppins",
     fontWeight: "bold",
     fontSize: "84px",
@@ -56,6 +73,10 @@ const useStyles = makeStyles({
   },
   cta: {
     marginTop: "30px",
+    textAlign: "center",
+    [theme.breakpoints.up('lg')]: {
+      textAlign:"left"
+    }
   },
   ctaMore: {
     "&:hover": {
@@ -72,32 +93,35 @@ const useStyles = makeStyles({
     marginLeft: "10px",
     "&:hover": {
       backgroundColor: "transparent",
-      },
-      height:'20px',
+    },
+    height: "20px",
   },
   playBtn: {
     marginTop: "25px",
     height: "100px",
     marginRight: "-25px",
   },
-});
+}));
 function Hero() {
   const css = useStyles();
   return (
-    <div className={css.main}>
-      <div className={css.left}>
+    <div >
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6} lg={6} className={css.left}>
+          
         <Typography variant="h5" className={css.tagLine}>
           Best Destinations around the world
-        </Typography>
-        <Typography variant="h1" className={css.heading}>
+          </Typography>
+          <Typography variant="h1" className={css.heading}>
           Travel, enjoy and live a new and full life
         </Typography>
         <Typography variant="p" className={css.desc}>
           Built Wicket longer admire do barton vanity itself do in it. Preferred
           to sportsmen it engrossed listening. Park gate sell they west hard for
           the.
-        </Typography>
-        <div className={css.cta}>
+          </Typography>
+          <div className={css.cta}>
+            
           <Button variant="contained" disableElevation className={css.ctaMore}>
             Find out more
           </Button>
@@ -117,14 +141,17 @@ function Hero() {
             Play demo
           </Button>
         </div>
-      </div>
-      <div className={css.right}>
+        </Grid>
+
+        <Grid item xs={12} md={4} lg={4} className={css.right}>
         <img
           className={css.img}
           src={process.env.PUBLIC_URL + "assets/heroImage.png"}
           alt=""
         />
-      </div>
+       </Grid>
+      </Grid>
+   
       <img
         className={css.bgBanner}
         src={process.env.PUBLIC_URL + "assets/bannerBG.png"}
@@ -135,6 +162,7 @@ function Hero() {
         src={process.env.PUBLIC_URL + "assets/deco.png"}
         alt="deco"
       />
+      
     </div>
   );
 }
